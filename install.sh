689ecfff19f0832e2768fc170d2d890954e73dc3
Fuqiang <frankdong80@gmail.com>
2018-06-23 10:22:46 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+yum install python-setuptools && easy_install pip
+pip install shadowsocks
+mkdir /etc/shdowsocks
+cp config.json /etc/shdowsocks/config.json
+nohup ssserver -c /etc/shadowsocks/config.json > /dev/null 2>&1 &
